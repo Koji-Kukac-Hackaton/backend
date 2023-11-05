10351c2ad424cd87b6b6ca02df0099d6c42231db
ianba <ian.balen6@gmail.com>
2023-11-05 08:08:07 +0100

    cors

--- src/main/java/com/codebooq/config/WebConfig.java
+++ src/main/java/com/codebooq/config/WebConfig.java
@@ -11,8 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")   // Allow any origin
                 .allowedMethods("*")   // Allow any HTTP method
-                .allowedHeaders("*")  // Allow any header
-                .exposedHeaders("Authorization") // Expose the Authorization header
-                .allowCredentials(true);
+                .allowedHeaders("Access-Control-Allow-Headers");  // Allow any header
     }
 }
