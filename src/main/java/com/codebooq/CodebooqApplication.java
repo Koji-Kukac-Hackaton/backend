1de2364c17d8db0af29ad2380845a5e1b59310ee
ianba <ian.balen6@gmail.com>
2023-11-05 03:51:39 +0100

    cors

--- src/main/java/com/codebooq/CodebooqApplication.java
+++ src/main/java/com/codebooq/CodebooqApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
 @EnableAsync
+@CrossOrigin
 public class CodebooqApplication {
 
     public static void main(String[] args) {
